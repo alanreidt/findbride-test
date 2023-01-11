437c171af48ee5ecec31b52e30293eee2556f141
Alan Reid <alanreidt@gmail.com>
2023-01-11 11:19:07 +0300

    Replace reducer by effector subscriptions

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,51 +1,34 @@
-import { AnyAction } from "redux";
-import { LinkedListNode } from "./types/LinkedListNode";
-import { Task, TaskStatus } from "./types/Task";
+import { TaskStatus } from "./types/Task";
 import {
-  addAction,
-  changeTaskStatusAction,
-  invertAction,
-  deleteAction,
+  taskAdded,
+  taskStatusChanged,
+  taskRemoved,
+  tasksInverted,
 } from "./actions";
 import { addTask, changeTaskStatus, deleteTask, invertTasks } from "./utils";
+import { $taskList } from "./store";
 
-type TasksState = {
-  headTask: LinkedListNode<Task> | null;
-};
-
-const initialState: TasksState = {
-  headTask: null,
-};
-
-export function tasksReducer(
-  state: TasksState = initialState,
-  action: AnyAction
-): TasksState {
-  if (addAction.match(action)) {
-    return {
-      ...state,
-      headTask: addTask(state.headTask, {
-        value: {
-          id: Date.now(),
-          name: action.payload,
-          status: TaskStatus.IN_PROGRESS,
-        },
-        next: null,
-      }),
-    };
-  } else if (changeTaskStatusAction.match(action)) {
-    return {
-      ...state,
-      headTask: changeTaskStatus(state.headTask, action.payload),
-    };
-  } else if (deleteAction.match(action)) {
-    return {
-      ...state,
-      headTask: deleteTask(state.headTask, action.payload),
-    };
-  } else if (invertAction.match(action)) {
-    return { ...state, headTask: invertTasks(state.headTask, null) };
-  } else {
-    return state;
-  }
-}
+$taskList
+  .on(taskAdded, (taskList, name) => ({
+    ...taskList,
+    headTask: addTask(taskList.headTask, {
+      value: {
+        id: Date.now(),
+        name,
+        status: TaskStatus.IN_PROGRESS,
+      },
+      next: null,
+    }),
+  }))
+  .on(taskStatusChanged, (taskList, taskState) => ({
+    ...taskList,
+    headTask: changeTaskStatus(taskList.headTask, taskState),
+  }))
+  .on(taskRemoved, (taskList, id) => ({
+    ...taskList,
+    headTask: deleteTask(taskList.headTask, id),
+  }))
+  .on(tasksInverted, (taskList) => ({
+    ...taskList,
+    headTask: invertTasks(taskList.headTask, null),
+  }));
