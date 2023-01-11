28d569d89d85feba7dc613d46850243a048e6766
Alan Reid <alanreidt@gmail.com>
2023-01-11 13:47:35 +0300

    Fix reaction on event calls

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,19 @@
 import { createStore } from "effector";
+import {
+  taskAdded,
+  taskRemoved,
+  tasksInverted,
+  taskStatusChanged,
+} from "./actions";
 import { LinkedListNode } from "./types/LinkedListNode";
-import { Task } from "./types/Task";
-import { linkedListToArr } from "./utils";
+import { Task, TaskStatus } from "./types/Task";
+import {
+  addTask,
+  changeTaskStatus,
+  deleteTask,
+  invertTasks,
+  linkedListToArr,
+} from "./utils";
 
 type TasksState = {
   headTask: LinkedListNode<Task> | null;
@@ -18,3 +30,28 @@ export const $tasks = $taskList.map((taskList) => {
 
   return headTask === null ? [] : linkedListToArr(headTask);
 });
+
+$taskList
+  .on(taskAdded, (taskList, name) => ({
+    ...taskList,
+    headTask: addTask(taskList.headTask, {
+      value: {
+        id: Date.now(),
+        name,
+        status: TaskStatus.IN_PROGRESS,
+      },
+      next: null,
+    }),
+  }))
+  .on(taskStatusChanged, (taskList, taskState) => ({
+    ...taskList,
+    headTask: changeTaskStatus(taskList.headTask, taskState),
+  }))
+  .on(taskRemoved, (taskList, id) => ({
+    ...taskList,
+    headTask: deleteTask(taskList.headTask, id),
+  }))
+  .on(tasksInverted, (taskList) => ({
+    ...taskList,
+    headTask: invertTasks(taskList.headTask, null),
+  }));
